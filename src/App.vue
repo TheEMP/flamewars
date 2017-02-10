be52180bcde471efdf91000b614b165ca95011e8
Your Name <Your email>
2017-02-10 12:16:35 -0700

    added vue-cli a whole new worllllllld

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return {
+      date:Date.now(),
+      list:Array.from({length:20}, (i,a)=>Math.random())
+    }
+  },
+  components:{
+    card
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  background:pink;
+}
+</style>
