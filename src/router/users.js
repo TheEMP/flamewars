b61f29934ba626696a58592e2ead897b4324b8b6
TheEMP <gunnarDebate@gmail.com>
2017-02-11 05:31:23 -0700

    More Stuff

--- src/router/users.js
+++ src/router/users.js
@@ -1,35 +1,27 @@
-<<<<<<< HEAD
+
 import userList from '../components/users/User-list.vue'
 import Profile from '../components/users/Profile'
 import Show from '../components/users/Show'
-=======
-
 import profile from '../components/users/Profile'
 import auth from "../components/users/Auth"
->>>>>>> 256c6c85e369cb9f74ef1ed5870bf1709e084b71
 import login from "../components/users/Login"
 import register from "../components/users/Register"
 export default [
-<<<<<<< HEAD
-    {
-      path: '/profile',
-      name: 'profile',
-      component: Profile
-    },
-    {
-      path: '/users',
-      name: 'users',
-      component: userList
-    },
-    {
-      path: '/user/:id',
-      name: 'user.show',
-      component: Show
-    },
-    {
-      path: "/login",
-      name: "login",
-=======
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
+  },
+  {
+    path: '/users',
+    name: 'users',
+    component: userList
+  },
+  {
+    path: '/user/:id',
+    name: 'user.show',
+    component: Show
+  },
   {
     path: '/profile',
     name: 'profile',
@@ -62,7 +54,6 @@ export default [
     }, {
       path: "login",
       name: "auth.login",
->>>>>>> 256c6c85e369cb9f74ef1ed5870bf1709e084b71
       component: login
     }]
   }
