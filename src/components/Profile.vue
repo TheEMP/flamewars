5f3c4af681ce38aa0a2e8d97f30cfb83824f097a
erikpshort <erikshort@hotmail.com>
2017-02-10 19:48:28 -0700

    Profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="profile-container">
         <!--<h1>{{user.name}}</h1>-->
-        <h1>hello</h1>
+        <h1>{{user.name}}</h1>
+        <img :src=""alt="">
         <threadlist :threads="evens"></threadlist>
         <div></div>
     </div>
@@ -18,7 +19,7 @@
                 user: {
                     id: 0,
                     name: "asdf",
-                    upvotes: 0,
+                    url: "http://",
                     downvotes: 0,
                 },
                 threads: Mock,
@@ -31,12 +32,11 @@
         computed: {
             evens() {
                 return this.threads.filter(function (t, i) {
-                    return (i % 2) 
-
-
+                    return (i == 2) 
                 })
+            },
 
-            }
+
         }
     }
 
